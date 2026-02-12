79d8f52eb4020c47847230b0261dc59fe1227523
no-t-a-coder <lionel.gymischueler@outlook.com>
2026-02-13 00:10:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-#include "helperFunctions.cpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "CHIP-8.h"
 
 int main(){
 
-    
+    CHIP8 chip8;
+    chip8.initialize();
+    chip8.load_program("opcode.bin");
+    chip8.FDE();
+
     return 0;
 
 }
