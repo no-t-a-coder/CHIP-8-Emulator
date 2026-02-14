3e4a8f6c2211b9a3c2945dda597492ad5a8c9ab7
no-t-a-coder <lionel.gymischueler@outlook.com>
2026-02-14 12:44:51 +0100

    Update helperFunctions.cpp

--- helperFunctions.cpp
+++ helperFunctions.cpp
@@ -1,4 +1,4 @@
-#include <string>
+
 #include "helperFunctions.h"
 //#include<iostream>
 
@@ -12,3 +12,42 @@ unsigned char biTOdec(std::string &input){
     //std::cout << value << "\n";
     return value;
 }
+
+std::string biTOhex(std::string &input){
+    
+    std::string hexRep;
+
+    if(input.length() % 4 != 0 || input.empty()){
+        throw std::invalid_argument("Binary Value not in shape for Hex conversion"); 
+    }
+
+    std::map<std::string, std::string> LookUP = {
+        {"0000" , "0"},
+        {"0001" , "1"},
+        {"0010" , "2"},
+        {"0011" , "3"},
+        {"0100" , "4"},
+        {"0101" , "5"},
+        {"0110" , "6"},
+        {"0111" , "7"},
+        {"1000" , "8"},
+        {"1001" , "9"},
+        {"1010" , "A"},
+        {"1011" , "B"},
+        {"1100" , "C"},
+        {"1101" , "D"},
+        {"1110" , "E"},
+        {"1111" , "F"}
+    };
+
+    for(unsigned long i = 0; i + 3 < input.length(); i += 4){
+        for(auto v : LookUP){
+            if(v.first == input.substr(i, 4)){
+                hexRep += v.second;
+            }
+        }
+    }
+    
+    return hexRep;
+
+}   
