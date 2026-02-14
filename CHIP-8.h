e53213456d942de1af7167632074ab4dff04d961
no-t-a-coder <lionel.gymischueler@outlook.com>
2026-02-14 12:45:38 +0100

    Update CHIP-8.h

--- CHIP-8.h
+++ CHIP-8.h
@@ -3,17 +3,20 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 
 class CHIP8{
     unsigned short pc;
     unsigned short sp;
-    unsigned short ip;
     unsigned short current_opcode;
+    unsigned short I;
 
     unsigned char memory[4096];
     unsigned char V[16];
     unsigned short stack[16];
+
+    unsigned char VF;
 
 
 public:
