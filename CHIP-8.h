d2b87834cafa70f21566ee5a25237e1f8c848f4a
no-t-a-coder <lionel.gymischueler@outlook.com>
2026-02-19 02:06:47 +0100

    Update CHIP-8.h

--- CHIP-8.h
+++ CHIP-8.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <SDL2/SDL.h>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -7,21 +8,36 @@
 
 
 class CHIP8{
+
+    
+
     unsigned short pc;
     unsigned short sp;
     unsigned short current_opcode;
     unsigned short I;
 
+    unsigned char delay_timer;
+    unsigned char sound_timer;
+
     unsigned char memory[4096];
     unsigned char V[16];
     unsigned short stack[16];
+    unsigned char gfx[32][64];
 
-    unsigned char VF;
-
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Event event;
+    
 
 public:
+
+    bool run;
+    unsigned char PixelScale;
+
+    ~CHIP8();
     void initialize();
     void load_program(std::string argfile);
-    void FDE();
+    void emulationCicle();
+    void updateScreen();
 
 };
