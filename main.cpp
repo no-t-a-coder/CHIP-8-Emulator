7dc271afbc1945d5484508de7ded6bcd94044889
no-t-a-coder <lionel.gymischueler@outlook.com>
2026-02-19 02:08:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include "CHIP-8.h"
-
+#include "helperFunctions.h"
 int main(){
+
 
     CHIP8 chip8;
     chip8.initialize();
     chip8.load_program("opcode.bin");
-    chip8.FDE();
+
+
+    while(chip8.run){
+        //emulation cicle
+        chip8.emulationCicle();
+        chip8.updateScreen();
+
+        
+    }
 
     return 0;
 
